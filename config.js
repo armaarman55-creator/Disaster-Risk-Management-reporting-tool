c1706d3cd8af941c4c106d0caaebf566b9a79094
armaarman55-creator <armaarman55@gmail.com>
2026-03-21 01:24:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@
 //   3. Copy "Project URL" and "anon / public" key
 
 window.DRMSA_CONFIG = {
-  SUPABASE_URL: '',        // e.g. https://abcdefgh.supabase.co
-  SUPABASE_ANON_KEY: '',   // your anon/public key (safe to expose in browsers)
+  SUPABASE_URL: '',        // https://olibqhpguquktrznchjm.supabase.co
+  SUPABASE_ANON_KEY: '',   // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9saWJxaHBndXF1a3Ryem5jaGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMTc4ODIsImV4cCI6MjA4OTU5Mzg4Mn0.O4q83EGZySNW5Gi_jcXC7yLQIYgvkDmNPjutO04ioag
 };
